c7f70659d014a2eb87379d95a26e290b6a19c6fb
Sahil Mondal <sahilmondal9879@gmail.com>
2025-05-06 23:36:56 +0530

    bugs

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -1,27 +1,31 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const path = require('path');
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+import taskRoutes from './routes/taskRoutes.js';
 
 dotenv.config();
 console.log("Loaded MONGODB_URI:", process.env.MONGODB_URI);
 connectDB();
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
 }));
 app.use(express.json());
 
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/tasks', require('./routes/taskRoutes'));
-app.use(express.static(path.join(__dirname, '/task-manager-frontend/dist')));
+app.use('/api/users', userRoutes);
+app.use('/api/tasks', taskRoutes);
+app.use(express.static(join(__dirname, '../task-manager-frontend/dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'task-manager-frontend', 'dist', 'index.html'));
+    res.sendFile(join(__dirname, '../task-manager-frontend/dist/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
