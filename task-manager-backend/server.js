544ed4f1531752d92e018e341d17a022e7f2825d
Sahil Mondal <sahilmondal9879@gmail.com>
2025-05-06 23:00:34 +0530

    bugs

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 
 dotenv.config();
 console.log("Loaded MONGODB_URI:", process.env.MONGODB_URI);
 connectDB();
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(cors({
@@ -16,6 +19,10 @@ app.use(express.json());
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use(express.static(path.join(__dirname, '/task-manager-frontend/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'task-manager-frontend', 'dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
